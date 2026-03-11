026c18f4464a081293485068bd47d1fee430782e
Wasuda Sandaruvan <147605725+WasuVa@users.noreply.github.com>
2026-03-11 23:43:29 +0530

    Add Flyway migration support and update security configuration
    - Introduced Flyway for database migrations with initial scripts for users and ingredients tables.
    - Enhanced security configuration to support CORS and role-based access control.
    - Updated role enumeration to include additional roles: CASHIER, WAITER, and CHEF.

---
+++ src/main/resources/db/migration/V2__ingredients.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS ingredients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    description TEXT
+);
