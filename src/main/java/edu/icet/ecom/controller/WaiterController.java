593dfae71362590e7d3b8799413d25063c407246
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-03-13 02:09:19 +0530

    Add serveOrder endpoint to WaiterController and update WaiterService interface

--- src/main/java/edu/icet/ecom/controller/WaiterController.java
+++ src/main/java/edu/icet/ecom/controller/WaiterController.java
@@ -2,10 +2,7 @@ package edu.icet.ecom.controller;
 
 import edu.icet.ecom.entity.OrderAssigment;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.codexa.retauranterp.service.WaiterService;
 
 import java.util.List;
@@ -21,5 +18,10 @@ public class WaiterController {
     @GetMapping("/unserved")
     public List<OrderAssigment> getUnservedOrders(){
         return waiterService.getUnservedOrders();
+    }
+
+    @PutMapping("/serve/{orderId}")
+    public void serveOrder(@PathVariable Long orderId){
+        waiterService.serveOrder(orderId);
     }
 }
