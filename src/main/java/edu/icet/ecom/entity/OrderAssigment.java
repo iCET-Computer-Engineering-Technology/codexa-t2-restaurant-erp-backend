70965b267b35608c6489408586328b3f5f01e95b
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-03-12 15:02:16 +0530

    init

---
+++ src/main/java/edu/icet/ecom/entity/OrderAssigment.java
@@ -0,0 +1,16 @@
+package edu.icet.ecom.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderAssigment {
+    private Long id;
+    private Long waiterId;
+    private Long orderId;
+    private String status;
+    private Long tableId;
+}
