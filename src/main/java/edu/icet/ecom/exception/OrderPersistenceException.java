c23b7a933d60fbc09872de07363589250549f67e
kalhara05 <kalhara139@gmail.com>
2026-03-14 10:36:26 +0530

    feat: add custom exception for order persistence errors

---
+++ src/main/java/edu/icet/ecom/exception/OrderPersistenceException.java
@@ -0,0 +1,8 @@
+package edu.icet.ecom.exception;
+
+public class OrderPersistenceException extends RuntimeException {
+    public OrderPersistenceException(String message) {
+        super(message);
+    }
+}
+
