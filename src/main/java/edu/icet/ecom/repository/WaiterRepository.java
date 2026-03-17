71c731cc9cd66c26431e1e5e664d0830964300d7
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-03-18 03:40:46 +0530

    Refactor WaiterRepository: remove unused methods for order assignment and unserved orders

--- src/main/java/edu/icet/ecom/repository/WaiterRepository.java
+++ src/main/java/edu/icet/ecom/repository/WaiterRepository.java
@@ -1,17 +1,9 @@
 package edu.icet.ecom.repository;
 
-import edu.icet.ecom.entity.OrderAssign;
 import edu.icet.ecom.entity.Waiter;
 
 import java.util.List;
 
 public interface WaiterRepository {
-    void assignWaiter(Long orderId, Long waiterId);
-
-    List<OrderAssign> getUnservedOrders(Long waiterId);
-
-
     List<Waiter> findActiveWaiters();
-
-    boolean markOrderServed(Long assignmentId);
 }
--- src/main/resources/db/migration/V7__assignment_status.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE order_assignments MODIFY COLUMN status VARCHAR(255) NOT NULL DEFAULT 'UNSERVED';
-
-UPDATE order_assignments SET status = 'UNSERVED' WHERE status = 'PENDING';
