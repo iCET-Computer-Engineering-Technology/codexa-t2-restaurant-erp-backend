6032c88ec0f91a4bf656c96dc9dd349bb20c79c4
AksaMJML <akaks0186@gmail.com>
2026-03-17 21:49:39 +0530

    feat: change CategoryRepositeryImpl annotation from @Service to @Repository

--- src/main/java/edu/icet/ecom/repository/impl/CategoryRepositeryImpl.java
+++ src/main/java/edu/icet/ecom/repository/impl/CategoryRepositeryImpl.java
@@ -4,11 +4,12 @@ import edu.icet.ecom.dto.CategoryDto;
 import edu.icet.ecom.repository.CategoryRepositery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Repository
 @RequiredArgsConstructor
 public class CategoryRepositeryImpl implements CategoryRepositery {
 
