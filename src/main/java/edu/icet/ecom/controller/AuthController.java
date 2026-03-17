d49b6365952aa7cd795a8d62f5e2f54f0808880e
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-03-17 17:34:09 +0530

    fix : 404 and 403 errors fixed - change access authenticated to permitAll for all request in SecurityConfig - remove @Validate annotations in api/auth/** APIs

--- src/main/java/edu/icet/ecom/controller/AuthController.java
+++ src/main/java/edu/icet/ecom/controller/AuthController.java
@@ -4,7 +4,6 @@ import edu.icet.ecom.dto.AuthResponse;
 import edu.icet.ecom.dto.LoginRequestDto;
 import edu.icet.ecom.dto.RegisterRequestDto;
 import edu.icet.ecom.service.AuthService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +18,12 @@ public class AuthController {
     private final AuthService service;
 
     @PostMapping("/register")
-    ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequestDto request){
+    ResponseEntity<AuthResponse> register(@RequestBody RegisterRequestDto request){
         return ResponseEntity.status(201).body(service.register(request));
     }
 
     @PostMapping("/login")
-    ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequestDto request){
+    ResponseEntity<AuthResponse> login(@RequestBody LoginRequestDto request){
         return ResponseEntity.status(200).body(service.login(request));
     }
 }
