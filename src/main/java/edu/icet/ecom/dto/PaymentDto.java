b1c28a6622c1f5ccc0f43d429247c0d96fa0647f
AksaMJML <akaks0186@gmail.com>
2026-03-29 20:17:51 +0530

    feat: add PaymentDto class for payment processing and update database password in configuration

---
+++ src/main/java/edu/icet/ecom/dto/PaymentDto.java
@@ -0,0 +1,20 @@
+package edu.icet.ecom.dto;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PaymentDto {
+    private Integer id;
+    private Integer orderId;
+    private String paymentMethod;
+    private Double amount;
+    private Double tipAmount;
+    private Integer processedBy;
+    private Timestamp processedAt;
+}
