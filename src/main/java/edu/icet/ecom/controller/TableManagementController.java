e9e983b55575f614f7e190e7e6069b01d3533040
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-04-07 21:38:23 +0530

    fix: update TableManagementController request mapping to standardize API endpoint

--- src/main/java/edu/icet/ecom/controller/TableManagementController.java
+++ src/main/java/edu/icet/ecom/controller/TableManagementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping({"/table-management", "/api/table-management"})
+@RequestMapping({ "/api/table-management"})
 @RequiredArgsConstructor
 @CrossOrigin
 public class TableManagementController {
